e724d25503ed877210aa8e85a522e18fcf23d868
John Mulhern <john@shihtzu.io>
2020-04-19 12:10:19 -0700

    added support for bable es6

--- src/login.js
+++ src/login.js
@@ -1,1 +1,9 @@
-console.log('logging in yo!')
+let login = (username, password) => {
+    if(username !== 'admin' || password !== 'radical') {
+        console.log('incorrect')
+    } else {
+        console.log('correct-o mundo')
+    }
+}
+
+login('admin', 'radical')
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
